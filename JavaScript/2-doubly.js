91648f4adf6c805c95a0ae0328f3acdb5ad8cc10
Mikhail Demidoff <demidoff.mikhail@gmail.com>
2016-10-21 00:58:45 +0300

    Implemented basic functionality.

--- JavaScript/2-doubly.js
+++ JavaScript/2-doubly.js
@@ -1,53 +1,119 @@
 'use strict';
 
-function LinkedList() {
-  this.first = null;
-  this.last = null;
-  this.length = 0;
+// Node of the doubly-linked list
+//   _data - value of the list element
+//     _prev - pointer to the previus node
+//     _next - pointer to the next node
+//
+function Node(data) {
+  this._data = data;
+  this._prev = null;
+  this._next = null;
 }
 
-LinkedList.prototype.push = function(data) {
-  let node = new Node(this, data);
-  node.prev = this.last;
-  if (this.length === 0) this.first = node;
-  else this.last.next = node;
-  this.last = node;
-  this.length++;
+// Implentation of the LinkedList
+//   args - array of values the LinkedList will be initialized with
+//     _length - size of the list
+//     _head - pointer to the first node
+//     _tail - pointer to the last node
+//
+function LinkedList(...args) {
+  this._length = 0;
+  this._head = null;
+  this._tail = null;
+  if (args.length > 0) this.extend(args);  
+}
+
+// Function for adding values to the end of the LinkedList
+//   data - value of the list element
+//
+LinkedList.prototype.append = function(data) {
+  let node = new Node(data);
+  node._prev = this._tail;
+  if (this._length === 0) this._head = node;
+  else this._tail._next = node;
+  this._tail = node;
+  this._length++;
   return node;
 };
 
-LinkedList.prototype.pop = function() {
-  if (this.length > 0) {
-    let node = this.last;
-    this.last = node.prev;
-    node.list = null;
-    node.prev = null;
-    node.next = null;
-    this.length--;
-    return node.data;
+// Function for extending current LinkedList with values
+//   args - array of values the LinkedList will be extended with
+//
+LinkedList.prototype.extend = function(...args) {
+  for (let i = 0; i < args.length; i++) {
+    this.append(args[i]);
   }
 };
 
-function Node(list, data) {
-  this.list = list;
-  this.data = data;
-  this.prev = null;
-  this.next = null;
-}
+// Function for printing current LinkedList
+//
+LinkedList.prototype.print = function() {
+  if (this._length > 0) {
+    let node = this._head;
+    console.dir(node._data);
+    while (node._next != null) {
+      node = node._next;
+      console.dir(node._data);
+    }
+  } else {
+    console.log('list is empty');
+  }
+};
 
-let list = new LinkedList();
-list.push({ name: 'first' });
-list.push({ name: 'second' });
-list.push({ name: 'third' });
+// Function that returns current LinkedList length
+//
+LinkedList.prototype.length = function() {
+  return this._length;
+};
 
-console.dir(list.pop());
-console.dir(list.pop());
-console.dir(list.pop());
-console.dir(list.pop());
+// Function for pulling last element from the LinkedList
+//
+LinkedList.prototype.pop = function() {
+  if (this._length > 0) {
+    let node = this._tail;
+    this._tail = node._prev;
+    node._prev = null;
+    node._next = null;
+    this._length--;
+    return node._data;
+  } else {
+    console.log('list is empty');
+  }
+};
 
-list.push({ name: 'uno' });
-list.push({ name: 'due' });
-console.dir(list.pop());
-list.push({ name: 'tre' });
-console.dir(list.pop());
-console.dir(list.pop());
+// Function that remove all elements from the LinkedList
+//
+LinkedList.prototype.clear = function() {
+  if (this._length > 0) {
+    let node = this._tail;
+    while (node != null) {
+      if (node._next != null) node._next = null;
+			   if (node._prev != null) node = node._prev;
+			   else node = null;
+    }
+    this._head = null;
+    this._tail = null;
+    this._length = 0;
+  } else {
+    console.log('list is empty');
+  }
+};
+
+// Function that swapping elements by their indices
+//   id1 - index of first element
+//   id2 - index of second element
+//
+LinkedList.prototype.swap = function(id1, id2) {
+  let node1 = this._head;
+  let node2 = this._head;
+  for (let i = 0; i < id1; i++) {
+    node1 = node1._next;
+  }
+  for (let j = 0; j < id2; j++) {
+    node2 = node2._next;
+  }
+  let tmp = node1._data;
+  node1._data = node2._data;
+  node2._data = tmp;
+};
