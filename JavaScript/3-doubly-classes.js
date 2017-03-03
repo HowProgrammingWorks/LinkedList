82003330a60c06a6b9407327779c0dc628a2ca85
ALEXKIRNAS <alexkirnas@ukr.net>
2017-03-03 21:32:14 +0200

    Code style. Rewriting methods pop and unshift. Adding map method and making class iterable.

--- JavaScript/3-doubly-classes.js
+++ JavaScript/3-doubly-classes.js
@@ -4,7 +4,7 @@ class Node {
   constructor(data, prev, next) {
     // If data type is Object then create a copy of data (don`t use reference)
     // else assign reference
-    if (typeof(data) === Object) {
+    if (typeof(data) === 'object') {
       this.data = Object.assign({}, data);
     } else {
       this.data = data;
@@ -17,79 +17,99 @@ class Node {
 class LinkedList {
   //Create empty list
   constructor() {
-    this.lenght = 0;
+    this.length = 0;
     this.head = null;
     this.tail = null;
   }
 
   // Adding new element to end of list
   append(data) {
-    if (this.lenght === 0)
+    if (this.length === 0) {
       this.head = this.tail = new Node(data);
-    else {
+    } else {
       this.tail.next = new Node(data, this.tail);
       this.tail = this.tail.next;
     }
 
-    this.lenght++;
+    this.length++;
   }
 
   // Deleting element from tail of list
   pop() {
-    if (this.lenght === 0) {
-      return null;
+    const tmp = this.tail;
+
+    if (this.length > 1) {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
     } else {
-      const tmp = this.tail;
+      this.tail = this.head = null;
+    }
 
-      if (this.lenght > 1) {
-        this.tail = this.tail.prev;
-        this.tail.next = null;
-      } else {
-        this.tail = this.head = null;
-      }
-
-      this.lenght--;
-      return tmp;
+    if (tmp) {
+      this.length--;
+      return tmp.data;
+    } else {
+      return null;
     }
   }
 
   // Adding new element to head of list
   shift(data) {
-    if (this.lenght === 0)
+    if (this.length === 0) {
       this.head = this.tail = new Node(data);
-    else {
+    } else {
       this.head.prev = new Node(data, null, this.head);
       this.head = this.head.prev;
     }
 
-    this.lenght++;
+    this.length++;
   }
 
   // Deleting element from head of list
   unshift() {
-    if (this.lenght === 0) {
-      return null;
-    } else {
-      const tmp = this.head;
+    const tmp = this.head;
 
-      if (this.lenght > 1) {
-        this.head = this.head.next;
-        this.head.prev = null;
-      } else {
-        this.tail = this.head = null;
-      }
-      this.lenght--;
-      return tmp;
+    if (this.length > 1) {
+      this.head = this.head.next;
+      this.head.prev = null;
+    } else {
+      this.tail = this.head = null;
+    }
+
+    if (tmp) {
+      this.length--;
+      return tmp.data;
+    } else {
+      return null;
     }
   }
-}
 
-// ???????? How to make definition of generator function inside class ?????
-function* iterator(list) {
-  let currNode = list.head;
-  while (currNode) {
-    yield currNode.data;
-    currNode = currNode.next;
+  // Make class List iterable
+  [Symbol.iterator]() {
+    let currNode = list.head;
+    return {
+      next: function() {
+        const tmp = currNode;
+        if (tmp) currNode = currNode.next;
+        return tmp ?
+          { value: tmp.data, done: false } :
+          { value: null, done: true };
+      }
+    };
+  }
+
+  // Applying function fn to all items of list
+  map(fn) {
+    if (fn && this.length) {
+      // We don`t use iterator here because data in Node isn`t always Object.
+      // So we can`t use reference assign.
+      let currNode = this.head;
+      while (currNode) {
+        currNode.data = fn(currNode.data);
+        currNode = currNode.next;
+      }
+    }
+    return this;
   }
 }
 
@@ -111,10 +131,22 @@ console.log();
 console.dir(list.unshift());
 
 console.log('\n-------------------------------------\n');
-for (let i = 0; i < 10; i++)
+for (let i = 0; i < 10; i++) {
   list.append(i);
+}
+
 
 // Showing all data using iterator
-let iter = iterator(list);
-for (let x of iter)
+for (let x of list) {
   console.log(x);
+}
+
+console.log('\n-------------------------------------\n');
+
+list
+  .map(x => x*x)
+  .map(x => x*2);
+
+for (let x of list) {
+  console.log(x);
+}
