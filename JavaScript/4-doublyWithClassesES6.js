411a8f7fe70704f2d918cb167faeaed246abf299
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-05 21:30:29 +0200

    doubly with ES6 classes

---
+++ JavaScript/4-doublyWithClassesES6.js
@@ -0,0 +1,60 @@
+'use strict';
+
+class LinkedList {
+
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  push(data) {
+    let currentNode = this.last;
+    const newNode = new Node(this, data);
+    newNode.prev = currentNode;
+    if (this.length === 0) this.first = newNode;
+    else currentNode.next = newNode;
+    this.last = newNode;
+    this.length++;
+    return newNode;  
+  }
+
+  pop() {
+    if (this.length > 0) {
+      const node = this.last;
+      this.last = node.prev;
+      if ( this.last !== null) this.last.next = null;
+      node.list = null;
+      node.prev = null;
+      this.length--;
+      return node.data;
+    }
+  }
+}
+
+class Node {
+  constructor(list, data) {
+    this.list = list;
+    this.data = data;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+
+const list = new LinkedList();
+list.push({ name: 'first' });
+list.push({ name: 'second' });
+list.push({ name: 'third' });
+
+console.dir(list.pop());
+console.dir(list.pop());
+console.dir(list.pop());
+console.dir(list.pop());
+
+list.push({ name: 'uno' });
+list.push({ name: 'due' });
+console.dir(list.pop());
+list.push({ name: 'tre' });
+console.dir(list.pop());
+console.dir(list.pop());
