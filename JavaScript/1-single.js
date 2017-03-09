5b2302d5d2025f12d2351e4c0c995ef141e0c64d
Dima Bulatov <bestdarkstar@gmail.com>
2017-03-09 15:10:08 +0200

    Added RightLinkedNode \ Added foreach in list

--- JavaScript/1-single.js
+++ JavaScript/1-single.js
@@ -1,14 +1,39 @@
 'use strict';
 
-function Node(prev, data) {
+function LeftLinkedNode(prev, data) {
   this.prev = prev;
   this.data = data;
 }
 
-const n1 = new Node(null, { name: 'first' });
-const n2 = new Node(n1, { name: 'second' });
-const n3 = new Node(n2, { name: 'third' });
+LeftLinkedNode.prototype.foreach = function(f) {
+  f(this.data);
+  if (this.prev !== null)
+    this.prev.foreach(f);
+};
 
-console.dir(n1);
-console.dir(n2);
-console.dir(n3);
+const ln1 = new LeftLinkedNode(null, { name: 'first' });
+const ln2 = new LeftLinkedNode(ln1, { name: 'second' });
+const ln3 = new LeftLinkedNode(ln2, { name: 'third' });
+
+ln3.foreach((d)=>{ console.dir(d); });
+
+function RightLinkedNode(prev, data) {
+  this.next = null;
+  if (prev !== null)
+    prev.next = this;
+  this.data = data;
+}
+
+RightLinkedNode.prototype.foreach = function(f) {
+  f(this.data);
+  if (this.next !== null)
+    this.next.foreach(f);
+};
+
+const rn1 = new RightLinkedNode(null, { name: 'first' });
+const rn2 = new RightLinkedNode(rn1, { name: 'second' });
+const rn3 = new RightLinkedNode(rn2, { name: 'third' });
+
+rn1.foreach((d)=>{ console.dir(d); });
+
+
