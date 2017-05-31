2cad573a2c68062b4498519e8b50cdf714445b15
Maks Melnik <maksmelnik0212@gmail.com>
2017-05-31 22:29:59 +0300

    Added CSharp examples

---
+++ CSharp/DoubleLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class DoubleLinkedList<T>
+    {
+        private Node last = null;
+
+        private class Node
+        {
+            public T item;
+            public Node next;
+            public Node previous;
+        }
+
+        public bool isEmpty()
+        {
+            return last == null;
+        }
+
+        public void addNode(T item)
+        {
+            Node oldLast = last;
+            last = new Node();
+            last.item = item;
+            last.previous = oldLast;
+            if (oldLast != null)
+            {
+                oldLast.next = last;
+            }
+        }
+
+        public T printList()
+        {
+            T item = default(T);
+            if (!isEmpty())
+            {
+                item = last.item;
+                last = last.previous;
+                if (last != null)
+                {
+                    last.next = null;
+                }
+            }
+            return item;
+        }
+    }
+}
