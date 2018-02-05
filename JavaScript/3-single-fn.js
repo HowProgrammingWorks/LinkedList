f238fb12d35dabdb1e98d11f9518c44e3443aa6d
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-06 00:54:04 +0200

    Single list with function closure

---
+++ JavaScript/3-single-fn.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const node = (data) => {
+  const element = (data) => {
+    const next = node(data);
+    next.prev = element;
+    return next;
+  };
+  element.data = data;
+  return element;
+};
+
+// Usage
+
+const obj1 = { name: 'first' };
+const obj2 = { name: 'second' };
+const obj3 = { name: 'third' };
+
+const list = node(obj1)(obj2)(obj3);
+
+console.dir(list);
