968c05f6723ae72074f5f891087a2fe7d2ecc054
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-06 00:55:28 +0200

    Single linked list with classes

---
+++ JavaScript/5-single-class.js
@@ -0,0 +1,26 @@
+'use strict';
+
+class List {
+  constructor() {
+    this.last = null;
+  }
+  push(data) {
+    const prev = this.last;
+    const element = { prev, data };
+    this.last = element;
+    return element;
+  }
+}
+
+// Usage
+
+const obj1 = { name: 'first' };
+const obj2 = { name: 'second' };
+const obj3 = { name: 'third' };
+
+const list = new List();
+list.push(obj1);
+list.push(obj2);
+list.push(obj3);
+
+console.dir(list.last);
