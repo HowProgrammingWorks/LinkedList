28b79bb2fadd93fd122900c97b4168f07dd97d51
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-06 00:54:51 +0200

    Iterable contract for single linked list

---
+++ JavaScript/4-single-iterable.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const list = () => {
+  let element;
+  return {
+    push(data) {
+      element = {
+        prev: element, data,
+      };
+      return element;
+    },
+    last: () => element,
+    [Symbol.iterator]: () => ({
+      current: element,
+      next() {
+        const element = this.current;
+        if (!element) return { done: true };
+        this.current = element.prev;
+        return {
+          value: element.data,
+          done: false
+        };
+      }
+    })
+  };
+};
+
+// Usage
+
+const obj1 = { name: 'first' };
+const obj2 = { name: 'second' };
+const obj3 = { name: 'third' };
+
+const l1 = list();
+l1.push(obj1);
+l1.push(obj2);
+l1.push(obj3);
+
+console.dir(l1.last());
+
+console.dir([...l1]);
+
+for (const element of l1) {
+  console.log(element);
+}
