6f87d4d21556db2a8191d1afc9d9b743fc2dc2b7
Viktoria Kosholap <victory.kosholap@gmail.com>
2018-02-09 22:35:33 +0200

    Implementation of tasks 1-4

---
+++ JavaScript/7-double-class.js
@@ -0,0 +1,54 @@
+'use strict';
+
+class LinkedList {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  push(data) {
+    const node = new Node(this, data);
+    node.prev = this.last;
+    if (this.length === 0) this.first = node;
+    else this.last.next = node;
+    this.last = node;
+    this.length++;
+    return node;
+  };
+  pop() {
+    if (this.length === 0) return null;
+    const node = this.last;
+    this.last = node.prev;
+    node.list = null;
+    node.prev = null;
+    node.next = null;
+    this.length--;
+    return node.data;
+  };
+}
+
+function Node(list, data) {
+  this.list = list;
+  this.data = data;
+  this.prev = null;
+  this.next = null;
+}
+
+// Usage
+
+const list = new LinkedList();
+list.push({ name: 'first' });
+list.push({ name: 'second' });
+list.push({ name: 'third' });
+
+console.dir(list.pop());
+console.dir(list.pop());
+console.dir(list.pop());
+console.dir(list.pop());
+
+list.push({ name: 'uno' });
+list.push({ name: 'due' });
+console.dir(list.pop());
+list.push({ name: 'tre' });
+console.dir(list.pop());
+console.dir(list.pop());
