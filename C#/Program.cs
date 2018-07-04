627e9033fabc6185e27e078fcf8e11677549da19
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2018-07-04 23:00:39 +0300

    Added project files

---
+++ C#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HowProgrammingWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
