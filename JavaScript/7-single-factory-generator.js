802e42efbffe22def8e034b466564dc6d69addfb
Serge Kovbasiuk <sergekovbasiuk@Serges-MacBook-Pro.local>
2020-01-06 11:39:16 +0800

    minor

--- JavaScript/7-single-factory-generator.js
+++ JavaScript/7-single-factory-generator.js
@@ -1,78 +1,78 @@
-const createNode = (data, next = null) => ({ data, next })
+'use strict';
+
+const createNode = (data, next = null) => ({ data, next });
 
 const createLinkedList = () => {
   const list = {
     head: null,
     length: 0,
     *traverse(node = list.head) {
-      yield node
-      if (node.next !== null && typeof (node.next) === "object") {
-        yield* list.traverse(node.next)
+      yield node;
+      if (node && node.next !== null && typeof(node.next) === 'object') {
+        yield* list.traverse(node.next);
       }
     },
     *[Symbol.iterator]() {
       for (const node of list.traverse()) {
-        yield node.data
+        yield node && node.data;
       }
     },
     append(data) {
       if (!list.head) {
-        list.head = createNode(data)
-        list.length++
+        list.head = createNode(data);
       } else {
         for (const node of list.traverse()) {
           if (!node.next) {
-            node.next = createNode(data)
-            list.length++
-            break
+            node.next = createNode(data);
+            break;
           }
         }
       }
+      list.length++;
     },
     find(data) {
       for (const node of list.traverse()) {
-        if (node.data === data) return node
+        if (node && node.data === data) return node;
       }
     },
     getNth(index) {
-      let count = 0
+      let count = 0;
       for (const node of list.traverse()) {
-        if (count === index) return node
-        count++
+        if (count === index) return node;
+        count++;
       }
     },
     prepend(data) {
-      const newNode = { data }
-      newNode.next = list.head
-      list.head = newNode
-      list.length++
+      const node = createNode(data, list.head);
+      list.head = node;
+      list.length++;
     },
     remove(data) {
-      let prev = null
+      let prev = null;
       for (const node of list.traverse()) {
-        if (node.data === data) {
+        if (node && node.data === data) {
           if (node === list.head) {
-            list.head = node.next
+            list.head = node.next;
           } else {
-            prev.next = node.next
+            prev.next = node.next;
           }
-          list.length--
+          list.length--;
         }
-        prev = node
+        prev = node;
       }
     },
-  }
+  };
 
-  return list
-}
+  return list;
+};
 
-const list = createLinkedList()
-list.append(1)
-list.append(2)
-list.append(3)
-list.prepend(4)
-list.remove(3)
-console.log(list.find(1)) // { data: 1 }
-console.log(list.getNth(0)) // { data: 4 }
-console.log(list.length) // 3
-console.log([...list]) // [4, 1, 2]
+const list = createLinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.prepend(4);
+list.remove(3);
+console.log(list.find(1)); // { data: 1 }
+console.log(list.getNth(0)); // { data: 4 }
+console.log(list.length); // 3
+console.log([...list]); // [4, 1, 2]
